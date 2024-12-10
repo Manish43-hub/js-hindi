7f06368ba49795cb08cbf3fc2b87086f6f2b69a7
Manish43-hub <manishbca43@gmail.com>
2024-12-10 07:38:16 +0000

    Two Types change

--- Basic/1-Variable.js
+++ Basic/1-Variable.js
@@ -5,4 +5,23 @@ accountCity = "patna"
 
 
 // accountId = 2 // Not allowed 
+
+accountEmail = "gnakf@gmail.com"
+accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
 console.log(accountId);
+
+/*    
+prever not to use var 
+because of issue in block scope and functional scope 
+
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
---
+++ Basic/2_Datatype.js
@@ -0,0 +1,5 @@
+" Use Strict"; // treat all js code as newer version 
+//alert("Hello!world")// we are using nodejs, not broswer
+
+console.log(3+3) 
+console.log("manish")
