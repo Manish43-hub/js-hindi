b9f1b5a8bb6a61524fd4af9b9ce42d61fb20997c
Manish43-hub <manishbca43@gmail.com>
2024-12-10 07:18:42 +0000

    variable

---
+++ Basic/1-Variable.js
@@ -0,0 +1,8 @@
+const accountId = 14254
+let accountEmail = "manishbca43@gmail.com"
+var accountPassword = "71222"
+accountCity = "patna"
+
+
+// accountId = 2 // Not allowed 
+console.log(accountId);
