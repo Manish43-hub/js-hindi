d090a905bb134a227211d2a8e298c201cf80953e
Manish43-hub <manishbca43@gmail.com>
2024-12-11 06:29:03 +0000

    conversion of datatype

--- Basic/3_conversionOperation.js
+++ Basic/3_conversionOperation.js
@@ -1,9 +1,34 @@
-let score = "33av"
-
-console.log(typeof score);
-console.log(typeof(score));
+let score = "manish"
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//"33" => 33
+// "33abc" => NaN (Not a Number)
+// "true" =>1; False => 0;
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = "manish"
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// Note :- 
+// 1 => true; 0=>false
+// ""=> false
+// "manish"=> true
+
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
